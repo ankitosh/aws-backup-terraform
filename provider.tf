4d3106e1577cdde93524c78c1d122ae7f8f55b72
Ankit Upadhyay <Ankitdigitek@gmail.com>
2021-08-08 12:16:19 +0300

    region-added

--- provider.tf
+++ provider.tf
@@ -1,9 +1,7 @@
-data "aws_caller_identity" "current" {}
-# provider "aws" {
-#   access_key = var.access_key
-#   secret_key = var.secret_key
-#   region     = var.region_name
-# }
+#data "aws_caller_identity" "current" {}
+provider "aws" {
+  region     = "eu-west-1"
+}
 # terraform {
 
 #   backend "s3" {
